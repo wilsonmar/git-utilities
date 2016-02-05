6fce42bfb20a798fcd9c97a7f0dfc4c85cb2a5fd
Wilson Mar <wilsonmar@gmail.com>
2016-02-04 16:39:48 -0800

    Update

--- git_move_filter.sh
+++ git_move_filter.sh
@@ -27,28 +27,28 @@ echo "*** User=$user, repoA=$repoA, repoB=$repoB, at $now."
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "*** Running from script_dir=$SCRIPT_DIR"
 
-echo "STEP 01: Get to the local working folder ready to receive the clone:"
+echo "*** STEP 01: Get to the local working folder ready to receive the clone:"
 cd ~
 rm -rf temp
 mkdir temp
 cd temp
 
-echo "STEP 02: Clone the originating repo you want to split to your local machine, specifying the branch:"
+echo "*** STEP 02: Clone the originating repo you want to split to your local machine, specifying the branch:"
 git clone $repoA $clone_folder # Create folder from Github
 cd $clone_folder
 dir=`pwd` # put results of pwd command into variable dir.
 echo "*** dir=$dir"
 
 
-echo "STEP 03: List files caz the next action promotes files in the directory up to the project root level:"
+echo "*** STEP 03: List files caz the next action promotes files in the directory up to the project root level:"
 ls -al
 
-echo "STEP 04: Avoid accidentally pushing changes by deleting the upstream repository definition in github:"
+echo "*** STEP 04: Avoid accidentally pushing changes by deleting the upstream repository definition in github:"
 git remote -v
 git remote rm origin
 
 echo "After this step, the local repo must be reset again using ./git_move_setup.sh."
-echo "STEP 05: Filter out all files except the one you want and at the same time "
+echo "*** STEP 05: Filter out all files except the one you want and at the same time "
 git filter-branch --prune-empty --subdirectory-filter $folderA1 -- --all
 #   The `--prune-empty` with `git filter-branch` brings over commits from **ONLY** the other repo which involves the directory being moved.
 #   The official doc at https://git-scm.com/docs/git-filter-branch
@@ -57,7 +57,7 @@ git filter-branch --prune-empty --subdirectory-filter $folderA1 -- --all
 #   An example of the response (where "directory 1" is replaced with your folder name):
 ls -al
 
-echo "STEP 06: Move contents of file raised to root back into a destination directory:"
+echo "*** STEP 06: Move contents of file raised to root back into a destination directory:"
 mkdir -p $dest_folder
 # TODO: Move more than just .txt files we know:
 find . -type f -exec git mv {} $dest_folder \;
@@ -66,11 +66,11 @@ pwd
 ls -al
 # git remote -v returns nothing here.
 
-echo "STEP 07: Commit to Github:"
+echo "*** STEP 07: Commit to Github:"
 git add .
 git commit -m"Move $folderA1 to $dest_folder in repo"
 
-echo "STEP 08: Clone the destination repo into ~/temp:"
+echo "*** STEP 08: Clone the destination repo into ~/temp:"
 cd ~
 cd temp
 rm -rf $repoB_folder
@@ -78,13 +78,13 @@ git clone $repoB $repoB_folder # Create folder from Github
 cd $repoB_folder
 pwd
 
-echo "STEP 09: Add _git_clone_repo_ as a location to pull from:"
+echo "*** STEP 09: Add _git_clone_repo_ as a location to pull from:"
 echo "${repoA}"
 git remote add repoA-branch ~/temp/${clone_folder}
 mkdir ~/temp/${repoB_folder}/${dest_folder}
 cd ${dest_folder}
 
-echo "STEP 10: Reset --hard to remove and avoid vim coming up:"
+echo "*** STEP 10: Reset --hard to remove and avoid vim coming up:"
 cd ~/temp/${repoB_folder}
 pwd
 git reset --hard
@@ -93,7 +93,7 @@ git pull       repoA-branch master
 
 git remote rm  repoA-branch
 
-echo "STEP 11: Commit to Github:"
+echo "*** STEP 11: Commit to Github:"
 git add .
 git commit -m"Move ${folderA1} in ${repoA} to ${repoB}."
 git remote -v
