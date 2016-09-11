ca349ef4f0d94aec255069368d7357641bbdfc38
Wilson Mar <wilsonmar+github@gmail.com>
2016-09-11 06:20:56 -0600

    Create git-sample-repo-create.sh

---
+++ git-sample-repo-create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# git-sample-repo-create.sh from within http://github.com/wilsonmar/git-utilities.
+# by Wilson Mar (wilsonmar@gmail.com)
+# This creates and populates a sample repo for my "Git and GitHub" tutorial.
+# For safety, the prior repo must be deleted manually.
+
+# Sample call:
+# chmod +x git-sample-repo-create.sh
+# /git-sample-repo-create.sh
