c8ee48ec1de6c85683a448d074c05f0394fb9caf
Wilson Mar <wilsonmar@gmail.com>
2016-12-10 04:17:09 -0700

    hooks install ps1 loop

--- git-hooks-install.ps1
+++ git-hooks-install.ps1
@@ -1,30 +1,35 @@
 # git-hooks-install.ps1
 # by wilsonmar at gmail.com
-# After the repo is cloned, this PowerShell script copies
-# scripts in the repo's hooks folder into the .git/hooks
+# After the repo is cloned, this PowerShell multi-platform script
+# copies scripts in the repo's hooks folder into the .git/hooks
 # and sets permissions.
 
-echo "Remove *.sample files in .git/hooks ..."
+#echo "Remove *.sample files in .git/hooks ..."
 Remove-Item .git/hooks/*.sample
 
-echo "Copy hooks/git-commit into .git/hooks  ..."
-
+#echo "Copy hooks/git-commit into .git/hooks  ..."
 Copy-Item hooks/*  .git/hooks
 #Copy-Item hooks/git-commit  .git/hooks
 #Copy-Item hooks/git-push  .git/hooks
 #Copy-Item hooks/git-rebase  .git/hooks
 #Copy-Item hooks/prepare-commit-msg .git/hooks
 
-if( $IsWindows -eq $True ) {
-   # icacls
-}
-if ( $IsOSX  -eq $True){
-   chmod +x .git/hooks/git-commit
-   chmod +x .git/hooks/git-push
-   chmod +x .git/hooks/git-rebase
-   chmod +x .git/hooks/prepare-commit-msg
+#echo "Change permissions  ..."
+$SUBDIR=".git/hooks"
+Get-ChildItem "$SUBDIR" -Filter *.log | 
+Foreach-Object {
+    $content = Get-Content $_.FullName
+    echo $content
+    if( $IsWindows -eq $True ) {
+       # attrib to set file permissions
+       # icacls to set ownership in Access Control Lists
+    }
+    if ( $IsOSX  -eq $True){
+       chmod +x $SUBDIR$content
+    }
 }
 
+#echo "Change permissions  ..."
 ls .git/hooks
 
-# echo "Done with status $? (0=OK)."
+echo "Done with status $? (0=OK)."
