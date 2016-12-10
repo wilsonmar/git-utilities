ab392b2047b7d487d8ce1a44cd2ec5b787d77445
Wilson Mar <wilsonmar@gmail.com>
2016-12-09 20:55:20 -0700

    add commit to install

--- git-hooks-install.ps1
+++ git-hooks-install.ps1
@@ -1,7 +1,7 @@
 # git-hooks-install.ps1
 # by wilsonmar at gmail.com
-# After the repo is cloned, this PowerShell script copies 
-# scripts in the repo's hooks folder into the .git/hooks 
+# After the repo is cloned, this PowerShell script copies
+# scripts in the repo's hooks folder into the .git/hooks
 # and sets permissions.
 
 echo "Remove *.sample files in .git/hooks ..."
@@ -9,19 +9,20 @@ Remove-Item .git/hooks/*.sample
 
 echo "Copy hooks/git-commit into .git/hooks  ..."
 
-Copy-Item hooks/prepare-commit-msg .git/hooks
+Copy-Item hooks/*  .git/hooks
 #Copy-Item hooks/git-commit  .git/hooks
-Copy-Item hooks/git-push  .git/hooks
-Copy-Item hooks/git-rebase  .git/hooks
+#Copy-Item hooks/git-push  .git/hooks
+#Copy-Item hooks/git-rebase  .git/hooks
+#Copy-Item hooks/prepare-commit-msg .git/hooks
 
 if( $IsWindows -eq $True ) {
    # icacls
 }
 if ( $IsOSX  -eq $True){
-   chmod +x .git/hooks/prepare-commit-msg
-   #chmod +x .git/hooks/git-commit
+   chmod +x .git/hooks/git-commit
    chmod +x .git/hooks/git-push
    chmod +x .git/hooks/git-rebase
+   chmod +x .git/hooks/prepare-commit-msg
 }
 
 ls .git/hooks
