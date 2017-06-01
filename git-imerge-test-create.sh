c12dfaa6ac5ecbd2193296effb04027c224966d3
Wilson Mar <wilsonmar@gmail.com>
2017-06-01 06:11:49 -0400

    Add imerge commands

--- git-imerge-test-create.sh
+++ git-imerge-test-create.sh
@@ -7,18 +7,18 @@
 # Before running this, copy this file to the folder where you want the test repo created,
 # then chmod 555 git-imerge-test-create.sh
 # Running this creates a folder above the script folder
-# and creates a file named somefile.md in both the floob branch and "frob" branch.
+# and creates a file named somefile.md in both the master branch and "feature1" branch.
 # This file is in https://github.com/wilsonmar/git-utilities/git-imerge-test-create.sh
 
 
 function conflict {  # $1=letter $2=cnt number
   x="$1$2"
   if [ $x = B2 ]||[ $x = G7 ]||[ $x = I9 ]; then 
-  	commit "$x frob" $1 frob
-  	commit "$x floob" $2 floob
+  	commit "$x feature1" $1 feature1
+  	commit "$x master" $2 master
   else
-  	commit "$x" $1 frob
-  	commit "$x" $2 floob
+  	commit "$x" $1 feature1
+  	commit "$x" $2 master
   fi
 }
 
@@ -47,8 +47,8 @@ echo "## Initializing repo with commit and branches:"
     touch somefile.md
 	git add somefile.md
 	git commit -m"0"
-    git branch frob
-    git branch floob
+    git branch feature1
+    git branch master
     git branch -avv
 
 echo "## Generating commits in branches:"
@@ -59,20 +59,39 @@ for i in ${letters[@]}; do
   cnt=$((cnt+1))  # increment
 done
 
-echo "## Verifying contents of branch frob and file somefile.md:"
+echo "## Verifying contents of branch feature1 and file somefile.md:"
 git log --pretty=format:"%h %s %ad" --graph --since=1.days --date=relative;
-git checkout frob
+git checkout feature1
 cat somefile.md
-echo "## Verifying contents of branch floob and file somefile.md:"
+echo "## Verifying contents of branch master and file somefile.md:"
 git log --pretty=format:"%h %s %ad" --graph --since=1.days --date=relative;
-git checkout floob
+git checkout master
 cat somefile.md
 git branch -avv
 #ls -al
 # You should now have:
-#    0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10 - 11 '  ← floob
+#    0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10 - 11 '  ← master
 #     \                                               
 #      A -- B -- C --- D --- E --- F --- G --- H --- I  ← branch
-git checkout frob
-git-imerge start --name=NAME --goal=full floob
+git checkout feature1
+git-imerge start --name=NAME --goal=full master
 git imerge diagram --name=NAME
+# sed -i "" "s/B2 feature1/B2 master/g" somefile.md
+#awk '{gsub(/pattern/,"replacement")}' file
+# TODO: Remove <<<<<<< HEAD, =======, line, and >>>>>>> 865fba2c721d68aca6486499f5e9cb33f95f5311
+# git add .
+# git commit -m"Fix B2"
+# git imerge continue
+  
+# TODO: Remove <<<<<<< HEAD, =======, line, and >>>>>>> 865fba2c721d68aca6486499f5e9cb33f95f5311
+# git add .
+# git commit -m"Fix G7"
+# git imerge continue
+
+# TODO: Remove <<<<<<< HEAD, =======, line, and >>>>>>> 865fba2c721d68aca6486499f5e9cb33f95f5311
+# git add .
+# git commit -m"Fix I9"
+# git imerge continue
+
+# git imerge finish
+# git imerge diagram --name=NAME
