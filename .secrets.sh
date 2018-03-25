82113bde58afd5abf51ac811cde0cabf2c609d06
Wilson Mar <WilsonMar+GitHub@hotmail.com>
2018-03-25 16:40:45 -0600

    Update

--- .secrets.sh
+++ .secrets.sh
@@ -6,8 +6,8 @@
 # Command source .secrets.sh pull these variables in.
 # CAUTION: No spaces around = sign.
 GIT_NAME="Wilson Mar"
-GIT_ID="WilsonMar@gmail.com"
-GIT_EMAIL="WilsonMar+GitHub@gmail.com"
+GIT_ID="WilsonMar@hotmail.com"
+GIT_EMAIL="WilsonMar+GitHub@hotmail.com"
 GIT_USERNAME="hotwilson"
 GPG_PASSPHRASE="only you know this 2 well"
 GITHUB_ACCOUNT="hotwilson"
