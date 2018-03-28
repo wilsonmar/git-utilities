3dbc52d60d51d99fe89f1d068a976f75e7f56a12
Wilson Mar <WilsonMar+GitHub@gmail.com>
2018-03-28 12:52:55 -0600

    add git hooks

--- gup.sh
+++
@@ -1,9 +0,0 @@
-# gup.sh
-# git update shell script, using push and pop of directories.
-
-pushd
-cd ~/gits/wilsonmar/futures
-git add .
-git commit -m"update via upg.sh"
-git push
-popd
--- sonar1.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# sonar.sh From https://github.com/wilsonmar/git-utilities
-
-chmod 555 sonar-scanner
-./sonar-scanner \
-  -Dsonar.projectKey=sonarqube-1-vm \
-  -Dsonar.sources=/Users/wilsonmar/gits/ng/angular4-docker-example \
-  -Dsonar.host.url=http://23.236.48.147  \
-  -Dsonar.login=7a51cb71a48ea3d16f57fe66021867fc2a98771e
