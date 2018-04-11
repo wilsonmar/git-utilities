baa5752ae1026eab234987276e58af9dfd2430bb
Wilson Mar <WilsonMar+GitHub@gmail.com>
2018-04-11 17:07:33 -0600

    update

--- tests/jenkins_secret_chrome.py
+++ tests/jenkins_secret_chrome.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # jenkins_secret_chrome.py in https://github.com/wilsonmar/git-utilities/tree/master/tests/
 # Call pattern:
-#    python tests/jenkins_secret_chrome.py   $JENKINS_PORT  $JENKINS_SECRET  jenkins.png
+# python tests/jenkins_secret_chrome.py  'chrome' $JENKINS_PORT  $JENKINS_SECRET  jenkins.png
 
 #import argparse  # https://docs.python.org/2/howto/argparse.html
 import sys
@@ -12,15 +12,7 @@ from random import randint
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def iso8601_utc():
-    class simple_utc(tzinfo):
-        def tzname(self,**kwargs):
-            return "UTC"
-        def utcoffset(self, dt):
-            return timedelta(0)
-    return datetime.utcnow().replace(tzinfo=simple_utc()).isoformat()
-    #print(iso8601_utc()+"   = ISO8601 time at +00:00 UTC (Zulu time), with microseconds")
-
+# From timestamps.py
 def iso8601_local():
     class local_tz(tzinfo):
         def utcoffset(self, dt):
@@ -30,18 +22,24 @@ def iso8601_local():
     return datetime.now().replace(microsecond=randint(0, 999999)).replace(tzinfo=local_tz()).isoformat()
     # print(iso8601_local()+" = ISO8601 time at local time zone offset, with random microseconds")
 
-jenkins_port=sys.argv[1]
-jenkins_secret=sys.argv[2]
+driver=sys.argv[1]
+jenkins_port=sys.argv[2]
+jenkins_secret=sys.argv[3]
 #picture_path=sys.argv[3]
-print('Params=', jenkins_port, jenkins_secret)
+print('driver=', driver, ', port=', jenkins_port, ', secret=', jenkins_secret)
 
 # TODO: #parser = argparse.ArgumentParser("simple_example")
 #parser.add_argument("counter", help="An integer will be increased by 1 and printed.", type=int)
 #args = parser.parse_args()
 #print(args.counter + 1)
 
-driver = webdriver.Chrome()
-#driver = webdriver.Firefox()
+if driver == "chrome":
+    print("chrome being used ...")
+    driver = webdriver.Chrome()
+elif driver == "firefox":
+    print("chrome being used ...")
+    driver = webdriver.Firefox()
+
 driver.get("http://localhost:" + jenkins_port) # TODO: pass port in
 assert "Jenkins [Jenkins]" in driver.title  # bail out if not found. Already processed.
 #time.sleep(5) # to see it.
@@ -50,10 +48,13 @@ assert "Jenkins [Jenkins]" in driver.title  # bail out if not found. Already pro
 secret = driver.find_element_by_id('security-token')
 secret.send_keys(jenkins_secret)
 secret.submit()
-time.sleep(10) # to give it time to work.
+time.sleep(8) # to give it time to work.
+
+# If the secret has already been processed:
+# INFO: Session node0vq5f7379gwo49h7hz67yatn37 already being invalidated
 
 # Take a picture (screen shot) of "Getting Started, Customize Jenkins"
-driver.save_screenshot('jenkins_secret_chrome.py' +iso8601_local()+ '.png')
+driver.save_screenshot('jenkins_secret_chrome.py.' +iso8601_local()+ '.png')
 assert "SetupWizard [Jenkins]" in driver.title 
 #time.sleep(5) # to see it
 
