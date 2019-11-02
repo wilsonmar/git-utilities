a30952ca292bc738e00419c80bc34193cbd3dbea
Wilson Mar <wilson_mar@mckinsey.com>
2019-11-02 09:30:21 -0400

    Add aliases

--- aliases.sh
+++ aliases.sh
@@ -3,21 +3,33 @@
 # Both called from ~/.bash_profile for Bash or ~/.zshrc for zsh
 # on both MacOS and git bash on Windows.
 
-EDITOR="code"  # subl = Sublime Text
+EDITOR="code"  # code = Visual Studio Code; subl = Sublime Text
 alias sbe="$EDITOR ~/.bash_profile"
 alias sbp='source ~/.bash_profile'
 alias rs='exec -l $SHELL'
 
+alias ..='cd ..'
 alias c="clear"  # screen
+alias h='history'
 alias x='exit'
 alias p="pwd"
-alias cf="find . -print | wc -l"  # count files in folder.
+alias j='jobs -l'
+
+alias now='date +"%T %d-%m-%Y"'
+alias epoch='date -u +%s'
+
 alias dir='ls -alrT'  # for windows habits
 alias l='ls -FalhGT'  # T for year
-alias last20='stat -f "%m%t%Sm %N" /tmp/* | sort -rn | head -20 | cut -f2-'
+alias lf="ls -p"      # list folders only
+alias cf="find . -print | wc -l"  # count files in folder.
+# Last 30 files updated anywhere:
+alias f30='stat -f "%m%t%Sm %N" /tmp/* | sort -rn | head -30 | cut -f2- 2>/dev/null'
+
+alias kp="ps auxwww"  # the "kp" alias ("que pasa")
+
 alias myip="ifconfig en0 | grep inet | grep -v inet6 | cut -d ' ' -f2"  # https://www.whatismyip.com/
 alias pubip="curl ifconfig.me"  # public IP
-alias pubip="curl http://canhazip.com"  # public IP also from slower "curl -s ifconfig.me" or curl https://checkip.amazonaws.com
+alias mac="curl http://canhazip.com"  # public IP also from slower "curl -s ifconfig.me" or curl https://checkip.amazonaws.com
 alias wanip4='dig @resolver1.opendns.com ANY myip.opendns.com +short'
 alias wanip6='dig @resolver1.opendns.com AAAA myip.opendns.com +short -6'
 alias ramfree="top -l 1 -s 0 | grep PhysMem"
@@ -51,5 +63,11 @@ alias tfa="terraform apply"
 alias tfd="terraform destroy"
 alias tfs="terraform show"
 
-alias dockx="docker stop $(docker ps -a -q);docker rm -f $(docker ps -a -q)"
-alias ports="sudo netstat -tulpn"  # mac
+# alias ports="sudo netstat -tulpn"  # mac
+
+#if command -v docker >/dev/null; then  # installed in /usr/local/bin/docker
+#   echo "Docker installed, so ..."
+#   alias dockx="docker stop $(docker ps -a -q);docker rm -f $(docker ps -a -q)"
+#fi
+
+# More: https://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
