21f7b904833a57289bab6ee144e5ec7185361144
Wilson Mar <wilsonmar+github@gmail.com>
2021-01-03 07:24:16 -0700

    Create my_github_accts_vars.sh

---
+++ my_github_accts_vars.sh
@@ -0,0 +1,7 @@
+# my_github_accts_vars.sh
+
+# This bash script reads flat file my_github_accts.csv to crate bash commands:
+# export LOCAL_SSH_KEYFILE="gmail_acct"
+# export MY_EMAIL_ADDRESS="johndoe@gmail.com"
+# export MY_GITHUB_ACCTNAME="johndoe"
+# export MY_FULL_NAME="John Doe"
